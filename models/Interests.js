b3ce5849384951e4420a55a27771bfa4c7f27cbd
paulina koch <pjassakoch@gmail.com>
2024-01-10 13:46:11 -0500

    added interest model

---
+++ models/Interests.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Interest extends Model {}
+
+
+Interest.init(
+  {
+    // Defining columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    interest_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'user',
+          key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'interest',
+  }
+);
+
+module.exports = Interest;
