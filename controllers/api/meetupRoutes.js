4d69371704b6a299a3e5765770d1986914fdef7a
Kunal Shah <you@example.com>
2024-01-11 14:35:55 -0500

    adding Meetup Route code

---
+++ controllers/api/meetupRoutes.js
@@ -0,0 +1,103 @@
+
+const router = require('express').Router();
+const { Interest, Meetup, User, UserMeetup } = require('../../models');
+
+
+// get all meetups
+router.get('/', async (req, res) => {
+    try {
+        const meetupData = await Meetup.findAll({
+            include: [
+                {
+                    model: User,
+                    through: UserMeetup,
+                },
+            ],
+        })
+
+        res.status(200).json(meetupData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get one meetup by id
+router.get('/:id', async (req, res) => {
+    try {
+        const meetupData = await Meetup.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    through: UserMeetup,
+                },
+            ],
+        })
+
+    if (!meetupData) {
+            res.status(404).json({ message: 'No events found with this id!' });
+            return;
+        }
+
+        res.status(200).json(meetupData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// create new meetup
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const meetupData = await Meetup.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = meetupData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(meetupData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+
+router.put('/:id', withAuth, async (req, res) => {
+    // update meetup data
+    try {
+        const meetupData = await Meetup.update(req.body, {
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        })
+
+        if (!meetupData) {
+            res.status(400).json({ message: "No meetup found with this information" })
+        }
+        res.status(200).json(meetupData)
+    }
+    catch (err) { res.status(500).json(err) }
+});
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+    // delete one meetup by its `id` value
+    try {
+        const meetupData = await Meetup.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if (!meetupData) {
+            res.status(404).json({ message: 'No meetup found with this information' });
+            return;
+        }
+        res.status(200).json(meetupData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
