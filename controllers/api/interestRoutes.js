13edb985a689e51ca6ec264f4c1afc7cd9b78906
BaharOssareh <78626041+BaharOssareh@users.noreply.github.com>
2024-01-15 11:42:20 -0500

    button heart is working now yaya :)

--- controllers/api/interestRoutes.js
+++ controllers/api/interestRoutes.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
 const { Interest, User, UserInterest } = require('../../models');
 const withAuth = require('../../utils/auth');
+const multer = require('multer');
+const path = require('path');
+
+
+const storage = multer.diskStorage({
+  destination: './public/upload/interests', 
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    cb(null, true);
+  },
+  limits: { fileSize: 5000000 }
+}).single('interest');
 
 //Get all interest
 router.get('/', withAuth, async (req, res) => {
@@ -16,6 +34,11 @@ router.get('/', withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   };
+});
+
+// router for rendering the page to create a new interest 
+router.get('/createInterest', (req, res) => {
+  res.render('createInterest');
 });
 
 //Get a single interest
@@ -38,6 +61,13 @@ router.get('/:id', withAuth, async (req, res) => {
     };
 
     const interest = interestData.get({ plain: true });
+
+    if (req.session.user_id) {
+      interest.userHasInterest = interest.users.some(user => user.id == req.session.user_id);
+    }
+    console.log(interest);
+    console.log(req.session.user_id);
+
     res.render('oneInterest', {
       interest,
       logged_in: req.session.logged_in
@@ -48,25 +78,31 @@ router.get('/:id', withAuth, async (req, res) => {
   };
 });
 
-//Create new interest
+// //Create new interest with/out image
 router.post('/', withAuth, async (req, res) => {
   try {
-    const interestData = await Interest.create(req.body);
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
 
-    // req.session.save(() => {
-    //   req.session.interest_id = interestData.id;
-    //   req.session.logged_in = true;
+      // Check if a file is selected
+      if (req.file) {
+        req.body.photo = `/upload/interests/${req.file.filename}`;
+      }
 
-    //   // Render the 'interest' view with the newly created interest data
-    //   res.render('interest', {
-    //     ...interestData.get({ plain: true }),
-    //     logged_in: true,
-    //   });
-    // });
-    res.status(200).json(interestData);
+      const interestData = await Interest.create(req.body);
+
+      req.session.save(() => {
+        req.session.interest_id = interestData.id;
+        req.session.logged_in = true;
+      });
+
+      res.status(200).json(interestData);
+    });
   } catch (err) {
-    res.status(400).json(err);
-  };
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 //Update an interest
@@ -98,10 +134,36 @@ router.put('/:id', withAuth, async (req, res) => {
         user_id: userId
       }
       await UserInterest.create(userInterestData);
-    }
+      console.log(userInterestData);
 
-    res.status(200).json(interestData);
+      const newUserInterest = await Interest.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            through: UserInterest,
+          },
+        ],
+      });
+      console.log(newUserInterest);
+
+      if (!newUserInterest) {
+        res.status(404).json({ message: 'No interest found with this id!' });
+        return;
+      };
+
+      const interest = newUserInterest.get({ plain: true });
+      res.render('oneInterest', {
+        where: {
+          interestId: req.params.id
+        },
+        interest,
+        logged_in: req.session.logged_in
+      })
+    } else {
+      res.status(200).json(interestData);
+    }
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   };
 });
